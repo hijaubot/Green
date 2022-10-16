0bc917f1f390ff1a6f6df2186a64262bffad1859
hijaubot <115866716+hijaubot@users.noreply.github.com>
2022-10-17 05:48:30 +0700

    Create Green.sh

---
+++ Green.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for n in {1..60};
+do
+    echo $n
+    echo $(date)>>Date.txt
+	  echo $(uuidgen)>>UUID.txt
+    git add .
+    git commit -m "a commit a day keeps your girlfriend away"
+    git push
+done
