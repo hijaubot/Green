bfc931aed60652158a59514d4352d37c418d32b7
hijaubot <115866716+hijaubot@users.noreply.github.com>
2022-10-17 05:48:46 +0700

    Update Green.sh

--- Green.sh
+++ Green.sh
@@ -3,7 +3,7 @@ for n in {1..60};
 do
     echo $n
     echo $(date)>>Date.txt
-	  echo $(uuidgen)>>UUID.txt
+    echo $(uuidgen)>>UUID.txt
     git add .
     git commit -m "a commit a day keeps your girlfriend away"
     git push
